28440f6115abd8c3d4b8594127fe537906868c48
Gregory Grubbs <gregory@dynapse.com>
2017-12-10 18:53:48 -0700

    Add defaults for CM SERVICEMONITOR

--- ingest/impala/99-setup-bay-area-bicycle-share.sql
+++ ingest/impala/99-setup-bay-area-bicycle-share.sql
@@ -1,4 +1,4 @@
--- [[file:~/projects/cloudera-demos/personalrepo-CIM-demos/README.org::*BABS%20-%20Bay%20Area%20Bike%20Share%20dataset][BABS - Bay Area Bike Share dataset:2]]
+-- [[file:~/projects/cloudera-demos/personalrepo-CIM-demos/README.org::*BABS%20-%20Bay%20Area%20Bikeshare][BABS - Bay Area Bikeshare:2]]
 -- can be run as Impala
 -- Bay Area Bicycle Share (BABS) dataset
   CREATE DATABASE IF NOT EXISTS bikeshare;
@@ -159,4 +159,4 @@
      CAST(wind_dir_degrees AS INT) AS wind_dir_degrees,
      zip
      FROM bikeshare.weather_raw;
--- BABS - Bay Area Bike Share dataset:2 ends here
+-- BABS - Bay Area Bikeshare:2 ends here
